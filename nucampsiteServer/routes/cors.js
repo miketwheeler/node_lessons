3797ac95784e6bfc4cd3677a02dd50311b4843da
miketwheeler <mikeytwheeler@gmail.com>
2022-04-20 23:54:54 -0700

    +to cors

--- nucampsiteServer/routes/cors.js
+++ nucampsiteServer/routes/cors.js
@@ -4,12 +4,12 @@ const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
 const corsOptionsDelegate = (req, callback) => {
     let corsOptions;
     console.log(req.header('Origin'));
-    corsOptions = (whitelist.indexOf(req.header('Origin')) !== -1) ? true : false;
-    // if(whitelist.indexOf(req.header('Origin')) !== -1) {
-    //     corsOptions = { origin: true };
-    // } else {
-    //     corsOptions = { origin: false };
-    // }
+    // corsOptions = (whitelist.indexOf(req.header('Origin')) !== -1);
+    if(whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true };
+    } else {
+        corsOptions = { origin: false };
+    }
     callback(null, corsOptions);
 }
 
