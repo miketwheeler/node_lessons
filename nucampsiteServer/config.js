9613fb23886d3922b1a406c0c3fe11938fd36ab8
miketwheeler <mikeytwheeler@gmail.com>
2022-04-21 23:33:08 -0700

    facebook auth

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '388291579837426',
+        clientSecret: '288661e21da8c7a8e41469e6c2c9aefa'
+    }
 }
